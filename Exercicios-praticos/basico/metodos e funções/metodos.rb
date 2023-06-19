361c9ac97ac3ce6bbe500beaa3fd644377de57b4
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-06-19 16:39:17 -0400

    [feat]: segundo dia de estudos e codificação mais simples para o trabalho

---
+++ Exercicios-praticos/basico/metodos e funções/metodos.rb
@@ -0,0 +1,17 @@
+def soma(a, b)
+  return a + b
+end
+
+resultado = soma(3, 5)
+puts resultado
+# Saída: 8
+
+def par?(numero)
+  return numero % 2 == 0
+end
+
+puts par?(4)
+# Saída: true
+
+puts par?(7)
+# Saída: false
